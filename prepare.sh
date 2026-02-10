6d00adbecd38e7ccc0683f792168eb0f99e7300d
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2026-02-11 00:08:00 +0100

    run.sh: move install to a separate script
    Makes easier to setup the environment using a different install script
    that will also run git submodule init if needed.
    
    Also, as the runner can't proceed without deps, check if it exists.
    If not, run git submodule init to create it.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

---
+++ prepare.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright (C) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+set -e
+info() {
+    echo "[prepare.sh] $1"
+}
+
+if [ ! -d "deps/benchkit/scripts/install_venv.sh" ]; then
+    git submodule update --init --recursive
+fi
+
+### Configure the env
+if test -d venv; then
+    info "benchmark environment already configured"
+else
+    info "configuring benchmark environment"
+    ./scripts/configure.sh
+fi
