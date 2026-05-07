0a12108ab5ad50970d21eb538dc5c140a25869b3
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2026-05-07 14:55:46 +0200

    config: add sysbench oltp_read_write bm-external test
    Add scripts and configuration to allow running sysbench
    oltp_read_write test with mariadb.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

---
+++ helpers/configure-sysbench.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Copyright (C) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+set -e
+SCRIPT_DIR="$(readlink -f $(dirname "$0")/../bm-external)"
+
+TAG=1.0.20
+
+mkdir -p ${SCRIPT_DIR}
+(
+	cd ${SCRIPT_DIR}
+	if [ ! -e sysbench/.git ]; then
+		git clone https://github.com/akopytov/sysbench --branch ${TAG}
+	else
+	    (cd sysbench && make clean|| true)
+	fi
+	cd sysbench
+	./autogen.sh
+	./configure --with-mysql --with-pgsql --prefix=${SCRIPT_DIR}/sysbench
+	make
+	make install
+)
