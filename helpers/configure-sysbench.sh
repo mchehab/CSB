f1dd50fb07fe7102c80038d7532d0de9e3719142
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2026-05-21 16:13:47 +0200

    helpers/configure-sysbench.sh: use a stable tag
    Picking latest version seems problematic, due to problems at
    configure.ac.
    
    Use a table one.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- helpers/configure-sysbench.sh
+++ helpers/configure-sysbench.sh
@@ -5,11 +5,13 @@
 set -e
 SCRIPT_DIR="$(readlink -f $(dirname "$0")/../bm-external)"
 
+TAG=1.0.20
+
 mkdir -p ${SCRIPT_DIR}
 (
 	cd ${SCRIPT_DIR}
 	if [ ! -e sysbench/.git ]; then
-		git clone https://github.com/akopytov/sysbench
+		git clone https://github.com/akopytov/sysbench --branch ${TAG}
 	else
 	    (cd sysbench && make clean|| true)
 	fi
