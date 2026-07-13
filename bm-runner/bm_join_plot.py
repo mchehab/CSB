66f5ceaed801dfbcea3c9922a0ce59849aa77698
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2026-07-13 15:38:22 +0200

    HACK
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- bm-runner/bm_join_plot.py
+++ bm-runner/bm_join_plot.py
@@ -362,9 +362,19 @@ def main() -> None:
                     plot_def["hue_lbl"] = "Kernel"
                     plot_def["palette"] = global_palette
 
+                    #########
+                    # FIXME #
+                    #########
                     # As hue is non-numerical, barplot won't work
                     if plot_def["shape"] == "barplot":
-                            plot_def["shape"] == "lineplot"
+                        plot_def["shape"] = "lineplot"
+
+                    # Ensure that the type will do what we need
+                    if not "type" in plot_def or plot_def["type"] == "normal":
+                        plot_def["type"] = "mean"
+
+                    # HACK
+                    print(plot_def)
 
                     plot = PlotConfig(**plot_def)
                     plot.title = f"{host}: {title} ({etype_name})"
